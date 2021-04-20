e38e0cc170fd3ce0ac485af71223a0d1f25d03ff
Peter Keenan <keenan@posteo.net>
2021-04-20 11:54:00 +0100

    Clean up and scaffold out components

--- movie-api.js
+++ movie-api.js
@@ -4,8 +4,7 @@ const req = axios.create({
   baseURL: 'https://api.themoviedb.org/3/movie',
 });
 
-export const fetchTopMovies = () => {
-  return req.get(`/top_rated?api_key=${apiKey}`).then(({ data }) => {
-    return data;
-  });
+export const fetchTopMovies = async () => {
+  const { data } = await req.get(`/top_rated?api_key=${apiKey}`);
+  return data;
 };
