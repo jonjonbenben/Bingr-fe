6485a7ce60a32b767fd063ced7c67ad0e17e31b4
Scott Oster <artificialingenious@gmail.com>
2021-04-22 12:59:58 +0100

    implement createGame into HostFilter component

--- components/HostFilter.jsx
+++ components/HostFilter.jsx
@@ -1,7 +1,9 @@
 import React, { useState } from 'react';
 import { Link, navigate } from '@reach/router';
 import '../public/HostFilter.css';
-import {initiateMovieList} from '../movieList'
+import { initiateMovieList } from '../movieList';
+import { formatGamesRoomMovies } from '../utils/utils';
+import { createGameRoom } from '../utils/createGameRoom';
 
 const HostFilter = (props) => {
   const [providers, setProviders] = useState([]);
@@ -41,102 +43,110 @@ const HostFilter = (props) => {
     // props.updateProviders(providers);
     props.setProviders(providers);
     props.setCategories(categories);
-    initiateMovieList().then((res)=> {
-      props.setMovieList(res)
-      navigate('/waitingroom');
-    })
-    
+    initiateMovieList()
+      .then((res) => {
+        props.setMovieList(res);
+        return res;
+      })
+      .then((filteredMovies) => {
+        const formattedMovies = formatGamesRoomMovies(filteredMovies);
+        return formattedMovies;
+      })
+      .then((formattedMovies) => {
+        createGameRoom('ghts', 'scott', formattedMovies);
+        navigate('/waitingroom');
+      });
   };
 
   return (
-    <div className="filter-page">
+    <div className='filter-page'>
       <h1>{`Hi ${props.name}!`}</h1>
 
-      <form className="filter-form" onSubmit={handleSubmit}>
+      <form className='filter-form' onSubmit={handleSubmit}>
         <h2>What are you watching on?</h2>
         <div>
           <input
             onChange={(e) =>
               handleChange(e.target.value, e.target.checked, e.target.className)
             }
-            type="checkbox"
-            id="netflix"
-            name="netflix"
-            value="8"
-            className="setProviders"
+            type='checkbox'
+            id='netflix'
+            name='netflix'
+            value='8'
+            className='setProviders'
           />
-          <label htmlFor="netflix">Netflix</label>
+          <label htmlFor='netflix'>Netflix</label>
           <br />
           <input
             onChange={(e) =>
               handleChange(e.target.value, e.target.checked, e.target.className)
             }
-            type="checkbox"
-            id="amazon"
-            name="amazon"
-            value="9"
-            className="setProviders"
+            type='checkbox'
+            id='amazon'
+            name='amazon'
+            value='9'
+            className='setProviders'
           />
-          <label htmlFor="amazon">Amazon Prime</label>
+          <label htmlFor='amazon'>Amazon Prime</label>
           <br />
           <input
             onChange={(e) =>
               handleChange(e.target.value, e.target.checked, e.target.className)
             }
-            type="checkbox"
-            id="disney"
-            name="disney"
-            value="337"
-            className="setProviders"
+            type='checkbox'
+            id='disney'
+            name='disney'
+            value='337'
+            className='setProviders'
           />
-          <label htmlFor="disney">Disney</label>
+          <label htmlFor='disney'>Disney</label>
         </div>
         <h2>Select a Genre</h2>
         <input
           onChange={(e) =>
             handleChange(e.target.value, e.target.checked, e.target.className)
           }
-          type="checkbox"
-          id="action"
-          name="action"
-          value="28"
-          className="setCategory"
+          type='checkbox'
+          id='action'
+          name='action'
+          value='28'
+          className='setCategory'
         />
-        <label htmlFor="action">Action</label>
+        <label htmlFor='action'>Action</label>
         <input
           onChange={(e) =>
             handleChange(e.target.value, e.target.checked, e.target.className)
           }
-          type="checkbox"
-          id="comedy"
-          name="comedy"
-          value="35"
-          className="setCategory"
+          type='checkbox'
+          id='comedy'
+          name='comedy'
+          value='35'
+          className='setCategory'
         />
-        <label htmlFor="comedy">Comedy</label>
+        <label htmlFor='comedy'>Comedy</label>
         <input
           onChange={(e) =>
             handleChange(e.target.value, e.target.checked, e.target.className)
           }
-          type="checkbox"
-          id="fantasy"
-          name="fantasy"
-          value="14"
-          className="setCategory"
+          type='checkbox'
+          id='fantasy'
+          name='fantasy'
+          value='14'
+          className='setCategory'
         />
-        <label htmlFor="fantasy">Fantasy</label>
+        <label htmlFor='fantasy'>Fantasy</label>
         <input
           onChange={(e) =>
             handleChange(e.target.value, e.target.checked, e.target.className)
           }
-          type="checkbox"
-          id="horror"
-          name="horror"
-          value="27"
-          className="setCategory"
+          type='checkbox'
+          id='horror'
+          name='horror'
+          value='27'
+          className='setCategory'
         />
-        <label htmlFor="horror">Horror</label>
-        <button className="button">Create Room</button>{' '}
+        <label htmlFor='horror'>Horror</label>
+        <button className='button'>Create Room</button>{' '}
       </form>
     </div>
   );
