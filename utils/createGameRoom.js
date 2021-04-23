9a7ab45f590c92119d25403edd26939a4ed2e2a9
Nathan <nathanmasters.com@hotmail.com>
2021-04-23 21:11:40 +0000

    db setup to have room code in col, movie id in doc, movie inside doc

--- utils/createGameRoom.js
+++ utils/createGameRoom.js
@@ -12,55 +12,45 @@ export const createGameRoom = (
 ) => {
   const batch = firebase.firestore().batch();
   const newCollection = firebase.firestore().collection(code);
-  firebase.firestore().collection(code).add();
-  // .then((res) => {
-  //   batch.set({ hello: 'darkness' });
 
-  //   // return firebase
-  //   //   .firestore()
-  //   //   .collection(code)
-  //   //   .doc(id)
-  //   //   .set({ hello: 'nan' });
-  // });
-
-  // const formattedBigMovieData = JSON.parse(JSON.stringify(bigMovieData));
-  // // console.log(Array.isArray(formattedBigMovieData));
-  // formattedBigMovieData.forEach((movie) => {
-  //   const {
-  //     adult,
-  //     backdrop_path,
-  //     genre_ids,
-  //     id,
-  //     original_title,
-  //     original_language,
-  //     overview,
-  //     popularity,
-  //     poster_path,
-  //     release_date,
-  //     title,
-  //     video,
-  //     vote_average,
-  //     vote_count,
-  //   } = movie;
-  //   firebase.firestore().collection(code).doc(id).set({
-  //     gran: 'dead',
-  //     grandad: 'prison',
-  //     adult,
-  //     backdrop_path,
-  //     genre_ids,
-  //     id,
-  //     original_title,
-  //     original_language,
-  //     overview,
-  //     popularity,
-  //     poster_path,
-  //     release_date,
-  //     title,
-  //     video,
-  //     vote_average,
-  //     vote_count,
-  //     increment_votes: 0,
-  //     tally: 0,
-  //   });
-  // });
+  bigMovieData.forEach((movie, index) => {
+    const movieId = bigMovieData[index].id;
+    const {
+      adult,
+      backdrop_path,
+      genre_ids,
+      id,
+      original_title,
+      original_language,
+      overview,
+      popularity,
+      poster_path,
+      release_date,
+      title,
+      video,
+      vote_average,
+      vote_count,
+    } = bigMovieData[index];
+    console.log(movieId.toString(), 'hereee');
+    firebase.firestore().collection(code).doc(movieId.toString()).set({
+      gran: 'dead',
+      grandad: 'prison',
+      adult,
+      backdrop_path,
+      genre_ids,
+      id,
+      original_title,
+      original_language,
+      overview,
+      popularity,
+      poster_path,
+      release_date,
+      title,
+      video,
+      vote_average,
+      vote_count,
+      increment_votes: 0,
+      tally: 0,
+    });
+  });
 };
